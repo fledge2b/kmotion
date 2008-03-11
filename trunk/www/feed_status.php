621cc41f659c47e4a59e157f4771020c6842cb65
dave6502 <dave6502@e039a933-3a47-0410-8b2d-f7601d26b524>
2008-03-11 19:20:19 +0000

    ********************* resync of SVN *********************************

---
+++ trunk/www/feed_status.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=us-ascii">
+	<link rel="stylesheet" type="text/css" href="base2.css">
+	<title>Kmotion</title>
+
+</head>
+<body id="feed_status">
+<!--
+Returns the latest jpeg filenames for all active video feeds plus any feeds identified in the
+events directory (ie feeds with motion detection active)
+
+The returned data is in a form that facilitates rapid Javascript string splitting
+-->
+<?php
+
+	$rc = file("./www.rc");
+	$path = rtrim($rc[0]);
+
+	$events = array();  // The events array ...
+	$dir = dir($path."/events/");  // Scan the events dir and add events to $events
+	while (false !== ($entry = $dir->read())) 
+	{
+		if ($entry !== "." and $entry !== "..") $events[] = $entry;
+	}
+	$dir->close();
+
+	sort($events, SORT_NUMERIC);
+
+	$rc = file("./www.rc");  // Read 1st line from feed.rc ... the number of feeds
+	$feeds = count($rc) - 1;
+
+	for ($i=1; $i < 17; $i++)
+	{
+		$jpeg_name = "";
+		if ($i <= $feeds)  // Skip jpeg names for feeds that do not exist
+		{
+			// Retrys reading $jpeg_holder if blank on the first try, possible cause being BASH
+			// script is re-writing $jpeg_holder. Avoids occasional white frames on video.
+			for ($j=0; $j<3; $j++)
+			{
+				$jpeg_holder = sprintf($path."/%s/%02d/last_jpeg", date("Ymd"), $i);
+				//$jpeg_holder = sprintf("/var/lib/motion/%s/%02d/last_jpeg", date("Ymd"), 1);  // usefull for simulating multiple feeds
+				if (file_exists($jpeg_holder))  // Abort of file does not exist
+				{
+					$jpeg_name = trim(file_get_contents($jpeg_holder));
+					# using '/images' as a key breakup the path & re-construct to match apache alias
+					$jpeg_name = explode('/images', $jpeg_name);
+					$jpeg_name = $jpeg_name[1];
+					$jpeg_name = '/images'.$jpeg_name;
+
+					if($jpeg_name !== "") break;
+					usleep(10);
+				}
+				else break;
+			}
+			echo "#".$jpeg_name;
+		}
+		else
+		{
+			echo "#";
+		}
+	}
+	echo "#";
+
+	foreach ($events as $event)
+	{
+		echo "$".$event;
+	}
+?>
---
+++ trunk/www/lowbw_frame.php
@@ -0,0 +1,256 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=us-ascii">
+	<link rel="stylesheet" type="text/css" href="base2.css">
+	<title>Kmotion</title>
+
+
+<script language="javascript">
+
+	// The Low Bandwidth frame ... display a jpeg until motion is detected
+
+	// The variables. Due to Javascript not having a sleep() function window.setTimeout() is used. This causes problems
+	// when trying to pass parameters resulting in a relatively high number of 'global' variables.  A closure construct is
+	// unworkable causing a recursion error.
+
+	parent_cache = {
+		view_seqs: parent.state.view_seqs,
+		view_format: parent.state.view_format,
+		video_feeds: parent.state.video_feeds,
+		pref_interleave: parent.state.interleave,
+		pref_motion_pause: parent.state.pref_motion_pause,
+		pref_normal_pause: parent.state.pref_normal_pause
+		};
+
+	stream = {
+		cache_jpeg: [],				// Caching jpeg array to avoid flicker
+		cache_num: 0,				// Count into above array
+		loaded: false,				// Status flag, true if image has been loaded
+
+		feed:0,					// Holds the feed pointer
+		start_time: 0,				// The start time in ms for the current view
+
+		server_reply1: [],			// An array of jpeg filenames returned by the AJAX call
+		server_reply2: [],			// An array of events ie feeds with motion detected returned by the AJAX call
+
+		server_snap2: [],			// A snapshot of the interleave feed array ... ie server_reply2
+		interleave_ptr:	1,			// Pointer into the snapshot server_snap2
+
+		cache_try_count: 0			// a caching try counter
+		};
+
+	stream.cache_jpeg[0] = new Image();
+	stream.cache_jpeg[1] = new Image();
+	stream.cache_jpeg[2] = new Image();
+	stream.cache_jpeg[3] = new Image();
+
+
+	//*******************************************************************************************************************************************************************************
+	// AJAX ...
+	//******************************************************************************************************************************************************************************
+
+	function server_poll()
+	{
+		var xmlHttpReq;
+		var response;
+		// Mozilla/Safari
+		if (window.XMLHttpRequest)
+		{
+			xmlHttpReq = new XMLHttpRequest();
+		}
+		// IE
+		else if (window.ActiveXObject)
+		{
+			xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlHttpReq.open("POST", "feed_status.php", true);
+		xmlHttpReq.send(null);
+		xmlHttpReq.onreadystatechange = function() 
+		{	
+			if (xmlHttpReq.readyState == 4)
+			{						// Array of latest jpeg filenames for feeds: index 1 - 16
+        			stream.server_reply1 = xmlHttpReq.responseText.split("#");
+									// Array of events: index 0 - length
+				stream.server_reply2 = stream.server_reply1[17].split("$");	
+			}
+		}
+	}
+
+
+	//*******************************************************************************************************************************************************************************
+	// Low Bandwidth polling loop
+	//******************************************************************************************************************************************************************************
+
+	function scan_motion()
+	{	
+		if ((stream.server_reply2.length) > 1)			// If there are events ...
+		{
+			// Snapshot the server_reply2 array in case it changes mid display
+			stream.snap_reply2 = stream.server_reply2;
+			var now = new Date();
+			stream.start_time = now.getTime();
+			stream_video();
+			return;
+		}
+		document.getElementById("text_1").innerHTML = ""; 	// Clear the text field 
+		document.getElementById("image_1").src = "misc/scanning.jpeg";
+		server_poll()
+		window.setTimeout("scan_motion()", 1000);
+	}
+
+
+	//*******************************************************************************************************************************************************************************
+	// Feed caching & display
+	//******************************************************************************************************************************************************************************
+
+	function stream_video()
+	{
+	
+		if (stream.interleave_ptr >= stream.snap_reply2.length)
+		{
+			stream.interleave_ptr = 1;				// End of an interleave cycle
+			window.setTimeout("scan_motion()", 1); 			// restart scan_motion()
+		}
+		else
+		{	
+			stream.feed = stream.snap_reply2[stream.interleave_ptr];
+			var now = new Date();					// Keep refreshing jpegs of current feed
+			if (now.getTime() > stream.start_time + 5000)		// for 5 seconds, then move to next feed
+			{
+				stream.start_time = now.getTime();
+				stream.interleave_ptr++;
+			}
+			server_poll();
+			cache();
+		}
+	}
+
+
+	function cache()
+	{
+		stream.cache_num++;  // caching as a browser workaround
+		stream.cache_num = (stream.cache_num > 3)?0:stream.cache_num;
+
+		stream.cache_jpeg[stream.cache_num].onload = function ()
+		{
+			set_view_text();
+			stream.cache_try_count = 0;
+			stream.loaded = true;
+		}
+
+		stream.cache_jpeg[stream.cache_num].onerror = function ()
+		{
+			stream.cache_try_count = 0;
+			stream.loaded = true;
+		}
+
+		// Avoid flashes of white on views
+		var feed_reply1 = stream.server_reply1[stream.feed];
+		var jpeg_file = (feed_reply1 == undefined)?"misc/caching.jpeg":feed_reply1;
+		if (feed_reply1 != "" && feed_reply1 != undefined)
+		{
+			document.getElementById("image_1").src = jpeg_file;
+			stream.cache_jpeg[stream.cache_num].src = jpeg_file;
+			cache_wait();
+		}
+		else
+		{
+			window.setTimeout("stream_video()", 1);
+		}
+	}	
+
+
+	function cache_wait()
+	{
+		stream.cache_try_count++;
+		if (stream.loaded)
+		{
+				stream.loaded = false;
+				var pause = Math.max(parent_cache.pref_motion_pause, 100);  // pause needed if browser accessing server on localhost, code in lowbw runns too fast 
+				window.setTimeout("stream_video();", pause);  // causing image freezing issues
+		}
+		else
+		{
+			if (stream.cache_try_count < 99)
+			{
+				window.setTimeout("cache_wait();", 30);
+			}
+			else
+			{
+				stream.cache_try_count = 0;
+				stream.loaded = false;
+				window.setTimeout("stream_video();", 1);
+			}
+		}
+	}
+
+
+	function set_view_text()
+	{
+		document.getElementById("text_1").innerHTML = stream.feed + " : " + parent.state.feed_text[stream.feed];
+		document.getElementById("text_1").style.color = "#ff0000";
+	}
+
+
+</script>
+</head>
+<body id="live_view">
+	<?php  
+
+	define("COLS", 1);
+	define("COL_PADDING", 3);
+
+	define("ROWS", 1);
+	define("ROW_PADDING", 3);
+
+	define("VIEW_WIDTH", $_GET['width']);
+	define("VIEW_HEIGHT", $_GET['height']);
+
+	// This is a black art, px never quite add up so may need to tweak +7	
+	$scale_width = VIEW_WIDTH;
+	$scale_height = VIEW_HEIGHT - ((ROW_PADDING * 2) + 7);
+
+	// Scale the jpeg keeping aspect ratio 384 / 288 = 1.33 
+	if (($scale_width / $scale_height) < 1.33)
+	{
+		$scale = $scale_width / (384 * (COLS - 1));
+		$scale_width = $scale * 384;
+		$scale_height =  $scale * 288;
+	}
+	else
+	{
+		$scale = $scale_height / (288 * ROWS);
+		$scale_width = $scale * 384;
+		$scale_height =  $scale * 288;		
+	}
+	
+	$col_width = $scale_width + COL_PADDING;
+	$total_cols_width = ($col_width * COLS) - COL_PADDING;
+	$lhs_margin = ((VIEW_WIDTH - $total_cols_width) / 2);
+
+	// Absolute positioning 
+	for ($row = 0; $row < ROWS; $row++)
+	{
+		for ($col = 0; $col < COLS; $col++)
+		{
+			$pos = ($col + 1) + ($row * COLS);
+			$camera_src = "misc/cashing.png";
+
+			printf("\n<img id=\"image_%d\" style=\"position:absolute;top:%dpx;left:%dpx;\"  src=\"%s\" width=%dpx height=%dpx onClick=\"view_clicked(%s);\" alt=\"Camera image\">", $pos, (($row + 1) * ROW_PADDING) + ($row * $scale_height), $lhs_margin + ($col * COL_PADDING) + ($col * $scale_width) , $camera_src,  $scale_width, $scale_height, $pos);
+
+			printf("\n<span id=\"text_%d\" style=\"position:absolute;top:%dpx;left:%dpx;\" alt=\"camera text\">", $pos, (($row + 1) * ROW_PADDING) + ($row * $scale_height)+ 3, $lhs_margin + ($col * COL_PADDING) + ($col * $scale_width) + 3);
+			printf("</span>");
+		}
+		printf("\n</div>\n");
+	}
+	?>
+ 
+<script language="JavaScript" type="text/javascript" >
+scan_motion();
+</script>
+
+</body>
