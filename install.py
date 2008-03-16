aff86812ff5a9ae3add8fa0a786479f55967c456
dave6502 <dave6502@e039a933-3a47-0410-8b2d-f7601d26b524>
2008-03-16 17:03:57 +0000

    - add /etc/hosts

--- install.py
+++ install.py
@@ -28,6 +28,7 @@ the only changes that will be made to your system are:
 (1) A link from /etc/apache2/sites-enabled/ to this directory
 (2) The addition of 'kmotion' and 'kmotion_restart' to /usr/bin/
 (3) The addition of 'sudo -u <name> motion &' to /etc/rc.local
+(4) The addition of '127.0.0.1 kmotion' to /etc/hosts
 
 All of which are reversible manually or by executing uninstall.py. 
 
@@ -117,9 +118,23 @@ Press ENTER to start install.\033[1;37m"""
     if ok: 
         print_ok()
     else: print_fail('Failed to add kmotion to \'/etc/rc.local\' please ensure \'/etc/rc.local\' has the line \'exit 0\' as the last line')
+    
+    # add 127.0.0.1 kmotion to /etc/hosts
+    print_checking('Adding \'127.0.0.1\' to \'/etc/hosts\'')
+    f = open('/etc/hosts', 'r')
+    lines = f.readlines()
+    f.close
+
+    f = open('/etc/hosts', 'w')
+    if lines[0] != '127.0.0.1\tkmotion\n':
+        f.write('127.0.0.1\tkmotion\n')
+    for line in lines:
+        f.write(line)
+    f.close
+    print_ok()
     print
     
-    
+   
 def print_checking(text):
     """
     Given a text string colorise to green and calculate the number of '.'s
