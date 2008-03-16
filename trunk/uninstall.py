ab8d1f6dc47d5328f1669db930672d90d6265bac
dave6502 <dave6502@e039a933-3a47-0410-8b2d-f7601d26b524>
2008-03-16 11:03:34 +0000

    * do not use * possible candidtae for v1.10b :)

--- trunk/uninstall.py
+++ trunk/uninstall.py
@@ -16,22 +16,19 @@
 
 import os, pwd, ConfigParser
 
-def install():
+def uninstall():
     """
-    A very simple automated install script ... this code is not bombproof !
+    A very simple automated uninstall script ... this code is not bombproof !
     """
-    
-    print """\033[1;32m
-Welcome to the kmotion v1.10b installer. Apart from internal configurations 
-the only changes that will be made to your system are:
+    print """\033[1;31m
+Welcome to the kmotion v1.10b uninstaller. Apart from the kmotion directory
+all traces of kmotion will be uninstalled. This includes
 
 (1) A link from /etc/apache2/sites-enabled/ to this directory
-(2) The addition of 'kmotion' and 'kmotion_restart' to /usr/bin/
-(3) The addition of 'sudo -u <name> motion &' to /etc/rc.local
+(2) The addition of 'kmotion' and 'kmotion_restart' in /usr/bin/
+(3) The addition of 'sudo -u <name> motion &' in /etc/rc.local\033[1;32m
 
-All of which are reversible manually or by executing uninstall.py. 
-
-Press ENTER to start install.\033[1;37m"""
+Press ENTER to start uninstall.\033[1;37m"""
 
     raw_input()
 
@@ -44,7 +41,7 @@ Press ENTER to start install.\033[1;37m"""
     print_ok()
     
     login = os.environ['SUDO_USER']
-
+    
     # check we can read ./daemons/daemon.rc - if we can't assume we are not in the root directory
     print_checking('Checking install is running in correct directory')
     parser = ConfigParser.SafeConfigParser()
@@ -54,52 +51,29 @@ Press ENTER to start install.\033[1;37m"""
         return
     print_ok()
     
-    # ironically then change to the daemons dir !
-    os.chdir('./daemons')
-        
-    # configure kmotion
-    print_checking('Generating kmotion configurations')
-    print_ok()
-    print_checking('Changing kmotion paths to cwd')
-    print_ok()
-    os.system('sudo -u %s ./install_utils.py' % login)
-    
-    images_dir = parser.get('dirs', 'images_dir')
-    misc_config_dir = parser.get('dirs', 'misc_config_dir')
-    daemons_dir = parser.get('dirs', 'daemons_dir')
-    apache2_config_dir = parser.get('dirs', 'apache2_config_dir')
-    www_dir = parser.get('dirs', 'www_dir')
-    
-    # apt-get critical packages
-    print_checking('Requesting critical packages')
-    print_ok()
-    print
-    os.system('apt-get install libapache2-mod-php5 apache2 motion screen')
-    
-    # link vhost file
-    print
-    print_checking('Linking apache2 \'sites-enabled\' to kmotion \'kmotion_vhost\'')
-    os.system('ln -fs %s/kmotion_vhost  /etc/apache2/sites-enabled' % apache2_config_dir)
-    print_ok()
-        
+    # delete link from /etc/apache2/sites-enabled/
+    print_checking('Deleteing apache2 \'sites-enabled\' link')
+    os.remove('/etc/apache2/sites-enabled/kmotion_vhost')
+    print_ok()  
+
     # restart apache2
     print_checking('Restarting apache2')
     print_ok()
     print
     os.system('/etc/init.d/apache2 restart')
+
+    # delete /usr/bin/kmotion
+    print_checking('Deleteing /usr/bin/kmotion')
+    os.remove('/usr/bin/kmotion')
+    print_ok()  
     
-    # move bin file kmotion to /usr/bin
-    print_checking('Adding \'kmotion\' to \'/usr/bin\'')
-    os.rename('%s/kmotion' % daemons_dir, '/usr/bin/kmotion')
-    print_ok()
-    
-    # move bin file kmotion_restart to /usr/local/bin
-    print_checking('Adding \'kmotion_restart\' to \'/usr/bin\'')
-    os.rename('%s/kmotion_restart' % daemons_dir,  '/usr/bin/kmotion_restart')
-    print_ok()
-            
-    # add kmotion to /etc/rc.local
-    print_checking('Adding \'kmotion\' to \'/etc/rc.local\' for auto startup')
+    # delete /usr/bin/kmotion_restart
+    print_checking('Deleteing /usr/bin/kmotion_restart')
+    os.remove('/usr/bin/kmotion_restart')
+    print_ok()  
+
+    # remove kmotion from /etc/rc.local
+    print_checking('Removing \'kmotion\' from \'/etc/rc.local\' for auto startup')
     f = open('/etc/rc.local', 'r')
     lines = f.readlines()
     f.close
@@ -108,17 +82,12 @@ Press ENTER to start install.\033[1;37m"""
     ok = False
     for line in lines:
         if line == 'sudo -u %s motion &\n' % login: continue
-        if line[:4] == 'exit':
-            f.write('sudo -u %s motion &\n' %  login)
-            ok = True
         f.write(line)
     f.close
-    if ok: 
-        print_ok()
-    else: print_fail('Failed to add kmotion to \'/etc/rc.local\' please ensure \'/etc/rc.local\' has the line \'exit 0\' as the last line')
+    print_ok()
     print
     
-    
+
 def print_checking(text):
     """
     Given a text string colorise to green and calculate the number of '.'s
@@ -140,5 +109,5 @@ def print_fail(text):
     """
     print '\033[1;31m[FAIL]\n\n%s\n\033[1;37m' % text,
     
-    
-install()
+
+uninstall()
