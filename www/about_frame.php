e5392e56dfd408bc5e666b332123243a7f5d2272
dave6502 <dave6502@e039a933-3a47-0410-8b2d-f7601d26b524>
2008-03-23 23:28:33 +0000

    - small fixes - struggling with vhost

--- www/about_frame.php
+++ www/about_frame.php
@@ -42,7 +42,8 @@ The Kmotion web interface
 </div>
 
 <div class="text">
-Kmotion v1.12b - Developed on a stock Gutsy Kubuntu 7.10, <br>Released under the GPL
+Kmotion v1.12b - Released under the GPL v2 - Copyright 2008 David Selby
+<br><br>Tested on stock Ubuntu 7.10<br>Tested on stock Ubuntu 8.04 (LTS)
 </div>
 
 <div class="section_heading text">
